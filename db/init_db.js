41f6412ca57147f780d53d84aaae8e71592e1c0f
Austin Lawrence <austin.lawrence.al@gmail.com>
2023-03-18 12:28:59 -0500

    added a date column to the database and initialization

--- db/init_db.js
+++ db/init_db.js
@@ -47,6 +47,7 @@ async function buildTables() {
           location VARCHAR(255) NOT NULL,
           "numHoles" INTEGER NOT NULL DEFAULT 1,
           "numFeet" INTEGER NOT NULL DEFAULT 20,
+          "jobDate" VARCHAR(255) NOT NULL,
           "rigId" INTEGER REFERENCES rigs(id)
       );
 
@@ -85,24 +86,24 @@ async function populateInitialData() {
       console.log("finished creating rigs!!");
 
       const jobsToCreate = [
-        {jobNumber: 'EWL-227', client: 'EWL', location: 'Plano, TX', numHoles: 3, numFeet: 60, rigId: 3},
-        {jobNumber: 'TER-321', client: 'TER', location: 'Sachse, TX', numHoles: 1, numFeet: 20, rigId: 2},
-        {jobNumber: 'AAA-111', client: 'AAA', location: 'Parker, TX', numHoles: 5, numFeet: 1000, rigId: 1},
-        {jobNumber: 'ZZZ-2626', client: 'ZZZ', location: 'Sea of Tranquility, Moon', numHoles: 3, numFeet: 120, rigId: 4},
-        {jobNumber: 'ZZZ-2627', client: 'ZZZ', location: 'Istanbul, Turkey', numHoles: 2, numFeet: 100, rigId: 4},
-        {jobNumber: 'ZZZ-2628', client: 'ZZZ', location: 'Berlin, Germany', numHoles: 4, numFeet: 80, rigId: 5},
-        {jobNumber: 'ZZZ-2629', client: 'ZZZ', location: 'Vancouver, Canada', numHoles: 4, numFeet: 80, rigId: 4},
-        {jobNumber: 'ZZZ-2630', client: 'ZZZ', location: 'Vancouver, Washington', numHoles: 3, numFeet: 60, rigId: 3},
-        {jobNumber: 'EWL-26', client: 'EWL', location: 'NY, NY', numHoles: 5, numFeet: 200, rigId: 4},
-        {jobNumber: 'EWL-2', client: 'EWL', location: 'Boston, MA', numHoles: 2, numFeet: 30, rigId: 6},
-        {jobNumber: 'ZZZ-6', client: 'ZZZ', location: 'Sacramento, CA', numHoles: 2, numFeet: 40, rigId: 4},
-        {jobNumber: 'EWL-666', client: 'EWL', location: 'Chicago, IL', numHoles: 1, numFeet: 20, rigId: 5},
-        {jobNumber: 'AAA-123', client: 'AAA', location: 'Ft. Worth, TX', numHoles: 3, numFeet: 60, rigId: 4},
-        {jobNumber: 'AAA-456', client: 'AAA', location: 'Dallas, TX', numHoles: 5, numFeet: 100, rigId: 1},
-        {jobNumber: 'AAA-100', client: 'AAA', location: 'Nowhere, KS', numHoles: 2, numFeet: 50, rigId: 4},
-        {jobNumber: 'AAA-200', client: 'AAA', location: 'St. Louis, MO', numHoles: 2, numFeet: 50, rigId: 2},
-        {jobNumber: 'AAA-300', client: 'AAA', location: 'Honolulu, Hawaii', numHoles: 2, numFeet: 50, rigId: 4},
-        {jobNumber: 'TER-7400', client: 'TER', location: 'Deep Space', numHoles: 2, numFeet: 50, rigId: 6},
+        {jobNumber: 'EWL-227', client: 'EWL', location: 'Plano, TX', numHoles: 3, numFeet: 60, jobDate: '2023-01-01', rigId: 3},
+        {jobNumber: 'TER-321', client: 'TER', location: 'Sachse, TX', numHoles: 1, numFeet: 20, jobDate: '2023-01-01', rigId: 2},
+        {jobNumber: 'AAA-111', client: 'AAA', location: 'Parker, TX', numHoles: 5, numFeet: 1000, jobDate: '2023-01-01', rigId: 1},
+        {jobNumber: 'ZZZ-2626', client: 'ZZZ', location: 'Sea of Tranquility, Moon', numHoles: 3, numFeet: 120, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'ZZZ-2627', client: 'ZZZ', location: 'Istanbul, Turkey', numHoles: 2, numFeet: 100, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'ZZZ-2628', client: 'ZZZ', location: 'Berlin, Germany', numHoles: 4, numFeet: 80, jobDate: '2023-01-01', rigId: 5},
+        {jobNumber: 'ZZZ-2629', client: 'ZZZ', location: 'Vancouver, Canada', numHoles: 4, numFeet: 80, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'ZZZ-2630', client: 'ZZZ', location: 'Vancouver, Washington', numHoles: 3, numFeet: 60, jobDate: '2023-01-01', rigId: 3},
+        {jobNumber: 'EWL-26', client: 'EWL', location: 'NY, NY', numHoles: 5, numFeet: 200, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'EWL-2', client: 'EWL', location: 'Boston, MA', numHoles: 2, numFeet: 30, jobDate: '2023-01-01', rigId: 6},
+        {jobNumber: 'ZZZ-6', client: 'ZZZ', location: 'Sacramento, CA', numHoles: 2, numFeet: 40, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'EWL-666', client: 'EWL', location: 'Chicago, IL', numHoles: 1, numFeet: 20, jobDate: '2023-01-01', rigId: 5},
+        {jobNumber: 'AAA-123', client: 'AAA', location: 'Ft. Worth, TX', numHoles: 3, numFeet: 60, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'AAA-456', client: 'AAA', location: 'Dallas, TX', numHoles: 5, numFeet: 100, jobDate: '2023-01-01', rigId: 1},
+        {jobNumber: 'AAA-100', client: 'AAA', location: 'Nowhere, KS', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'AAA-200', client: 'AAA', location: 'St. Louis, MO', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 2},
+        {jobNumber: 'AAA-300', client: 'AAA', location: 'Honolulu, Hawaii', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 4},
+        {jobNumber: 'TER-7400', client: 'TER', location: 'Deep Space', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 6},
       ];
 
       console.log("creating jobs");
