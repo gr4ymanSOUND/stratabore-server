6eb25910cb435b9e241b493a1df9b945fc022982
Austin Lawrence <austin.lawrence.al@gmail.com>
2023-05-09 18:32:53 -0500

    added a jobLength and createdDate field to the job table; added new jobs and updated existing jobs in the initDB file

--- db/init_db.js
+++ db/init_db.js
@@ -46,14 +46,16 @@ async function buildTables() {
         
       CREATE TABLE jobs (
           id SERIAL PRIMARY KEY,
-          "jobNumber" VARCHAR(255) NOT NULL,
+          "jobNumber" VARCHAR(255) UNIQUE NOT NULL,
           client VARCHAR(255) NOT NULL,
           location VARCHAR(255) NOT NULL,
           "numHoles" INTEGER NOT NULL DEFAULT 1,
           "numFeet" INTEGER NOT NULL DEFAULT 20,
-          "jobDate" VARCHAR(255) NOT NULL,
+          "jobDate" VARCHAR(255),
+          "jobLength" INTEGER NOT NULL DEFAULT 1,
           "rigId" INTEGER REFERENCES rigs(id),
-          status VARCHAR(255) NOT NULL
+          status VARCHAR(255) NOT NULL,
+          "createdDate" VARCHAR(255) NOT NULL,
       );
 
       
@@ -91,24 +93,33 @@ async function populateInitialData() {
       console.log("finished creating rigs!!");
 
       const jobsToCreate = [
-        {jobNumber: 'EWL-227', client: 'EWL', location: 'Plano, TX', numHoles: 3, numFeet: 60, jobDate: '2023-04-02', rigId: 3, status: 'pending'},
-        {jobNumber: 'TER-321', client: 'TER', location: 'Sachse, TX', numHoles: 1, numFeet: 20, jobDate: '2023-04-05', rigId: 2, status: 'pending'},
-        {jobNumber: 'AAA-111', client: 'AAA', location: 'Parker, TX', numHoles: 5, numFeet: 1000, jobDate: '2023-04-04', rigId: 1, status: 'pending'},
-        {jobNumber: 'ZZZ-2626', client: 'ZZZ', location: 'Sea of Tranquility, Moon', numHoles: 3, numFeet: 120, jobDate: '2023-01-01', rigId: 4, status: 'pending'},
-        {jobNumber: 'ZZZ-2627', client: 'ZZZ', location: 'Istanbul, Turkey', numHoles: 2, numFeet: 100, jobDate: '2023-04-02', rigId: 4, status: 'pending'},
-        {jobNumber: 'ZZZ-2628', client: 'ZZZ', location: 'Berlin, Germany', numHoles: 4, numFeet: 80, jobDate: '2023-04-02', rigId: 5, status: 'pending'},
-        {jobNumber: 'ZZZ-2629', client: 'ZZZ', location: 'Vancouver, Canada', numHoles: 4, numFeet: 80, jobDate: '2023-04-04', rigId: 4, status: 'complete'},
-        {jobNumber: 'ZZZ-2630', client: 'ZZZ', location: 'Vancouver, Washington', numHoles: 3, numFeet: 60, jobDate: '2023-04-04', rigId: 3, status: 'pending'},
-        {jobNumber: 'EWL-26', client: 'EWL', location: 'NY, NY', numHoles: 5, numFeet: 200, jobDate: '2023-04-05', rigId: 4, status: 'pending'},
-        {jobNumber: 'EWL-2', client: 'EWL', location: 'Boston, MA', numHoles: 2, numFeet: 30, jobDate: '2023-04-03', rigId: 6, status: 'pending'},
-        {jobNumber: 'ZZZ-6', client: 'ZZZ', location: 'Sacramento, CA', numHoles: 2, numFeet: 40, jobDate: '2023-04-03', rigId: 4, status: 'canceled'},
-        {jobNumber: 'EWL-666', client: 'EWL', location: 'Chicago, IL', numHoles: 1, numFeet: 20, jobDate: '2023-01-01', rigId: 5, status: 'canceled'},
-        {jobNumber: 'AAA-123', client: 'AAA', location: 'Ft. Worth, TX', numHoles: 3, numFeet: 60, jobDate: '2023-01-01', rigId: 4, status: 'pending'},
-        {jobNumber: 'AAA-456', client: 'AAA', location: 'Dallas, TX', numHoles: 5, numFeet: 100, jobDate: '2023-01-01', rigId: 1, status: 'pending'},
-        {jobNumber: 'AAA-100', client: 'AAA', location: 'Nowhere, KS', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 4, status: 'pending'},
-        {jobNumber: 'AAA-200', client: 'AAA', location: 'St. Louis, MO', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 2, status: 'pending'},
-        {jobNumber: 'AAA-300', client: 'AAA', location: 'Honolulu, Hawaii', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 4, status: 'pending'},
-        {jobNumber: 'TER-7400', client: 'TER', location: 'Deep Space', numHoles: 2, numFeet: 50, jobDate: '2023-01-01', rigId: 6, status: 'pending'},
+        {jobNumber: 'EWL-227', client: 'EWL', location: 'Plano, TX', numHoles: 3, numFeet: 60, jobDate: '2023-05-02', jobLength: 1, rigId: 3, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'TER-321', client: 'TER', location: 'Sachse, TX', numHoles: 1, numFeet: 20, jobDate: '2023-05-05', jobLength: 2, rigId: 2, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-111', client: 'AAA', location: 'Parker, TX', numHoles: 5, numFeet: 1000, jobDate: '2023-05-04', jobLength: 2, rigId: 1, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-2626', client: 'ZZZ', location: 'Lucas, TX', numHoles: 3, numFeet: 120, jobDate: '2023-05-01', jobLength: 1, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-2627', client: 'ZZZ', location: 'Frisco, TX', numHoles: 2, numFeet: 100, jobDate: '2023-05-02', jobLength: 2, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-2628', client: 'ZZZ', location: 'McKinney, TX', numHoles: 4, numFeet: 80, jobDate: '2023-05-02', jobLength: 1, rigId: 5, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-2629', client: 'ZZZ', location: 'Mansfield, TX', numHoles: 4, numFeet: 80, jobDate: '2023-05-04', jobLength: 1, rigId: 4, status: 'complete', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-2630', client: 'ZZZ', location: 'Mesquite, TX', numHoles: 3, numFeet: 60, jobDate: '2023-05-04', jobLength: 1, rigId: 3, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'EWL-26', client: 'EWL', location: 'Rose Hill, TX', numHoles: 5, numFeet: 200, jobDate: '2023-05-05', jobLength: 2, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'EWL-2', client: 'EWL', location: 'Rowlett, TX', numHoles: 2, numFeet: 30, jobDate: '2023-05-23', jobLength: 1, rigId: 6, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-6', client: 'ZZZ', location: 'Saginaw, TX', numHoles: 2, numFeet: 40, jobDate: '2023-05-11', jobLength: 2, rigId: 4, status: 'canceled', createdDate: '2023-01-01'},
+        {jobNumber: 'EWL-666', client: 'EWL', location: 'Burleson, TX', numHoles: 1, numFeet: 20, jobDate: '2023-05-01', jobLength: 1, rigId: 5, status: 'canceled', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-123', client: 'AAA', location: 'Ft. Worth, TX', numHoles: 3, numFeet: 60, jobDate: '2023-05-12', jobLength: 1, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-456', client: 'AAA', location: 'Dallas, TX', numHoles: 5, numFeet: 100, jobDate: '2023-05-14', jobLength: 2, rigId: 1, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-100', client: 'AAA', location: 'Bedford, TX', numHoles: 2, numFeet: 50, jobDate: '2023-05-25', jobLength: 1, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-200', client: 'AAA', location: 'Cedar Hill, TX', numHoles: 2, numFeet: 50, jobDate: '2023-05-26', jobLength: 1, rigId: 2, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-300', client: 'AAA', location: 'Desoto, TX', numHoles: 2, numFeet: 50, jobDate: '2023-05-30', jobLength: 1, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'TER-7400', client: 'TER', location: 'Haltom City, TX', numHoles: 2, numFeet: 50, jobDate: '2023-05-28', jobLength: 2, rigId: 6, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'TER-222', client: 'TER', location: 'Grand Prairie, TX', numHoles: 1, numFeet: 25, jobDate: '2023-05-11', jobLength: 1, rigId: 1, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'TER-987', client: 'TER', location: 'Lewisville, TX', numHoles: 3, numFeet: 75, jobDate: '2023-05-12', jobLength: 2, rigId: 3, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'EWL-5454', client: 'EWL', location: 'Carrolton, TX', numHoles: 4, numFeet: 100, jobDate: '2023-05-09', jobLength: 2, rigId: 2, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'EWL-1234', client: 'EWL', location: 'Flower Mound, TX', numHoles: 1, numFeet: 20, jobDate: '2023-05-07', jobLength: 1, rigId: 5, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-999', client: 'ZZZ', location: 'Justin, TX', numHoles: 2, numFeet: 40, jobDate: '2023-05-14', jobLength: 1, rigId: 4, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'ZZZ-889', client: 'ZZZ', location: 'Prosper, TX', numHoles: 1, numFeet: 20, jobDate: '2023-05-22', jobLength: 1, rigId: 3, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-112', client: 'AAA', location: 'Grapevine, TX', numHoles: 1, numFeet: 50, jobDate: '2023-05-24', jobLength: 1, rigId: 1, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'AAA-567', client: 'AAA', location: 'Crowley, TX', numHoles: 2, numFeet: 100, jobDate: '2023-05-29', jobLength: 2, rigId: 2, status: 'pending', createdDate: '2023-01-01'},
+        {jobNumber: 'EWL-876', client: 'EWL', location: 'Midlothian, TX', numHoles: 4, numFeet: 200, jobDate: '2023-05-19', jobLength: 2, rigId: 6, status: 'pending', createdDate: '2023-01-01'},
       ];
 
       console.log("creating jobs");
